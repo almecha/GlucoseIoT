14fbebb247352aee8ab9c24bb94d4fbf83d30374
almecha <nurzhanaitaliyev@gmail.com>
2025-05-31 14:46:58 +0200

    feat:reports generator connected to Thingspeak Adaptor via REST

--- reports_generator/reports_generator.py
+++ reports_generator/reports_generator.py
@@ -12,61 +12,14 @@ import pandas as pd
 
 class ReportsGenerator(object):
     def __init__(self, catalog_file_path):
-        self.catalog_file_path = catalog_file_path
-        self.catalog = json.load(open(self.catalog_file_path, encoding='utf-8'))
-        self.patientList = self.catalog["patientsList"]
-        self.serviceDetails = self.catalog["serviceDetails"]
-        self.base_url = self.serviceDetails[2]["REST_endpoint"]
-        self.access_code = "1234"
+        # self.catalog_file_path = catalog_file_path
+        # self.catalog = json.load(open(self.catalog_file_path, encoding='utf-8'))
+        # self.patientList = self.catalog["patientsList"]
+        # self.serviceDetails = self.catalog["serviceDetails"]
+        self.base_url = "127.0.0.1:8080/retrieve"  # Base URL for the REST API
         self.NUMBER_OF_ENTRIES_PER_REQUEST = 100
-        self.THINGSPEAK_USER_API_KEY = "TO BE DONE LATER"
-        self.BASE_URL = "https://api.thingspeak.com/channels"
-        self.ACCESS_CODE = "1234"
 
-    def generate_report(self, patientID):
-        """
-        Generates a report for the given patient ID by fetching data from the REST API.
-        """
-        if patientID not in range(len(self.patientList)):
-            return "Error: Patient ID is not valid"
-        # Fetch data from thingspeak
-        df = self.read_json_from_thingspeak(patientID)
-        # Check if the DataFrame is empty
-        if df.empty:
-            return "No data available for the given patient ID."
-        # Calculate the metrics
-        glucose_measurements = df["glucose_level"].to_list()
-        avg_glucose = sum(glucose_measurements) / len(glucose_measurements)
-        min_glucose = min(glucose_measurements)
-        max_glucose = max(glucose_measurements)
-        tir_metics = self.calculate_time_in_range(glucose_measurements)
-        variability_metrics = self.calculate_variability(glucose_measurements)
 
-    def read_json_from_thingspeak(self, patientID):
-        """
-        Read JSON data from the Thingspeak channel via REST API.
-        Called on page refresh...
-        """
-        channel_id = self._user_api_keys(patientID)
-        url = f"{self.BASE_URL}/{channel_id}/feeds.json?api_key={self.THINGSPEAK_USER_API_KEY}&results={self.NUMBER_OF_ENTRIES_PER_REQUEST}"
-        response = requests.get(url, timeout=5)  # Send GET request to the URL
-        
-        if response.status_code == 200:
-            data = response.json()  # Parse JSON response
-            df = pd.DataFrame(data['feeds'])  # Convert 'feeds' to DataFrame
-            return df
-
-        return (f"Failed to fetch data. Status code: {response.status_code}")
-    
-    def _user_api_keys(self, patient_id):
-        """
-        To extract user API keys from the catalog.
-        """
-
-        if patient_id not in range(len(self.patientList)):
-            return "Error: Patient ID is not valid"
-        
-        return self.patientList[patient_id]["serviceDetails"]["Thingspeak"]["channelAPIkey"]
     
     def calculate_time_in_range(self, glucose_measurements):
         """
@@ -118,3 +71,61 @@ class ReportsGenerator(object):
             "Coefficient of Variation (CV)": cv,
             "Glucose Management Indicator (GMI)": gmi
         }
+    
+    def generate_report(self, patientID):
+        """
+        Generates a report for the given patient ID by fetching data from the REST API.
+        """
+        # if patientID not in range(len(self.patientList)):
+        #     return "Error: Patient ID is not valid"
+        # Fetch data from thingspeak
+        response = requests.get(
+            f"{self.base_url}?number_of_entries={self.NUMBER_OF_ENTRIES_PER_REQUEST}",
+        )
+        data = response.json()
+        data = json.loads(data)["feeds"]
+        # Check if the DataFrame is empty
+        if data == None:
+            return "No data available for the given patient ID."
+        # Calculate the metrics
+        glucose_measurements = [float(reading["field1"]) for reading in data if "field1" in reading]
+        avg_glucose = sum(glucose_measurements) / len(glucose_measurements)
+        min_glucose = min(glucose_measurements)
+        max_glucose = max(glucose_measurements)
+        tir_metics = self.calculate_time_in_range(glucose_measurements)
+        variability_metrics = self.calculate_glucose_variability(glucose_measurements)
+        report = json.dumps({
+            "Patient ID": patientID,
+            "Average Glucose": avg_glucose,
+            "Minimum Glucose": min_glucose,
+            "Maximum Glucose": max_glucose,
+            "Time in Range Metrics": tir_metics,
+            "Glucose Variability Metrics": variability_metrics
+        })
+        return report
+
+    def GET(self, *uri, **params):
+        """
+        Handle GET requests to generate the report.
+        """
+        if len(uri) == 0:
+            return "No arguments provided"
+        elif uri[0] == "generate_report":
+            # Return the report as a JSON response
+            report = self.generate_report(int(params.get('patientID', 0)))
+            return json.dumps(report)
+        else:
+            return "Unknown endpoint"
+
+if __name__ == "__main__":
+    web_service = ReportsGenerator("catalog.json")
+    conf={
+        '/':{
+        'request.dispatch':cherrypy.dispatch.MethodDispatcher(),
+        'tools.sessions.on':True
+        }
+        }
+    cherrypy.tree.mount(web_service,'/',conf)
+    cherrypy.config.update({'server.socket_port':8080})
+    cherrypy.engine.start()
+    cherrypy.engine.block()
