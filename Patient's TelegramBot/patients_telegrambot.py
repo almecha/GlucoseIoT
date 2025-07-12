127c510f615c559b9911f1f19bcbc29f28a007db
Sol-liendo <s337960@studenti.polito.it>
2025-07-12 11:16:29 +0200

    Create patients_telegrambot.py

---
+++ Patient's TelegramBot/patients_telegrambot.py
@@ -0,0 +1,8 @@
+from MyMQTT import *
+import telepot
+from telepot.loop import MessageLoop
+import json
+import cherrypy
+from datetime import datetime, timedelta, timezone
+from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
+
